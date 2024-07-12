6e069aa81ad273a2efad9b8c9c5c7db39efc4c4a
PrajwalBK <prajwalbkhavatkopp@gmail.com>
2024-07-12 16:37:12 +0530

    comit

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='Mlops',
-    author='Anmol',
+    description='Demo',
+    author='prajwal',
     license='MIT',
 )
